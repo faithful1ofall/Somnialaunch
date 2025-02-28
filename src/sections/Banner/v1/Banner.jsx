317268d3c967c4bc944b02a7ce983b868d77f91b
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-02-28 14:45:46 +0100

    Update Banner.jsx

--- src/sections/Banner/v1/Banner.jsx
+++ src/sections/Banner/v1/Banner.jsx
@@ -15,14 +15,14 @@ const Banner = () => {
               alt="banner icon"
             />
             <h1 className="banner-title">
-              Metaverse Web 3.0 Gaming Launcepad &amp; IGO
+              Mini Launch Metaverse Web 3.0 NFT Agent Launchpad &amp; AI
             </h1>
             <div className="description">
-              The next generation gaming ecosystem for IGOs and NFT
+              The next generation A.I ecosystem for NFT AI Agents
             </div>
 
             <Button href="/projects-grid" variant="mint" md isCenter className="banner-btn">
-              Explore IGO
+              Explore Collections
             </Button>
           </div>
         </div>
