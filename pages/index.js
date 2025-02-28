5f76c87052d894be518304577398e9734abd3e8b
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-02-28 12:09:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,9 @@ export default function Home() {
         <About />
         <NextProjects />
         <PreviousProjects />
-        <Statistics />
+          {/*   <Statistics />
         <Team />
-        <Partner />
+        <Partner />*/}
         <Footer />
       </Layout>
     </Fragment>
