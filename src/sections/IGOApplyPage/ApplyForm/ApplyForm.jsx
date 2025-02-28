071c40a0b0f85977fce2463f4f649bcced1e6e2a
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-02-28 20:00:59 +0100

    Update ApplyForm.jsx

--- src/sections/IGOApplyPage/ApplyForm/ApplyForm.jsx
+++ src/sections/IGOApplyPage/ApplyForm/ApplyForm.jsx
@@ -1,110 +1,150 @@
-import { FaTelegramPlane, FaTwitter } from "react-icons/fa"
+import { useState } from "react";
+import { FaTelegramPlane, FaTwitter, FaUpload, FaMagic } from "react-icons/fa";
 import Button from "@components/button";
-import ApplyFormStyleWrapper from "./ApplyFrom.style"
+import ApplyFormStyleWrapper from "./ApplyFrom.style";
+import axios from "axios";
 
 const ApplyForm = () => {
+    const [layers, setLayers] = useState([]);
+    const [nftCount, setNftCount] = useState(1);
+
+    const handleLayerUpload = (event, index) => {
+        const files = event.target.files;
+        if (files.length > 0) {
+            const newLayers = [...layers];
+            newLayers[index].image = files[0];
+            setLayers(newLayers);
+        }
+    };
+
+    const handleTraitChange = (index, field, value) => {
+        const newLayers = [...layers];
+        newLayers[index][field] = value;
+        setLayers(newLayers);
+    };
+
+    const addLayer = () => {
+        setLayers([...layers, { name: "", rarity: "", image: null }]);
+    };
+
+    const uploadToIPFS = async (files, type) => {
+        const formData = new FormData();
+        for (let file of files) {
+            formData.append("file", file);
+        }
+
+        const pinataOptions = JSON.stringify({ cidVersion: 0 });
+        formData.append("pinataOptions", pinataOptions);
+
+        try {
+            const response = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": `Bearer YOUR_PINATA_JWT`
+                }
+            });
+            return response.data.IpfsHash;
+        } catch (error) {
+            console.error(`Error uploading ${type} to IPFS:`, error);
+            return null;
+        }
+    };
+
+    const generateNFTs = async () => {
+        const imageFiles = layers.map(layer => layer.image);
+        const imageCID = await uploadToIPFS(imageFiles, "images");
+
+        if (!imageCID) return alert("Failed to upload images");
+
+        const metadataFiles = layers.map((layer, index) => ({
+            name: `NFT #${index + 1}`,
+            attributes: [{ trait_type: layer.name, value: layer.rarity }],
+            image: `ipfs://${imageCID}/${index + 1}.png`
+        }));
+
+        const metadataBlob = new Blob([JSON.stringify(metadataFiles, null, 2)], { type: "application/json" });
+        const metadataCID = await uploadToIPFS([metadataBlob], "metadata");
+
+        if (!metadataCID) return alert("Failed to upload metadata");
+
+        alert(`NFTs generated! Images: ipfs://${imageCID}, Metadata: ipfs://${metadataCID}`);
+    };
+
     return (
         <ApplyFormStyleWrapper>
             <form>
+                {/* Existing Collection Form Fields */}
                 <div className="form_widgets">
                     <div className="form-group">
                         <label htmlFor="CollectionName">Collection Name</label>
                         <input type="text" id="CollectionName" placeholder="Collection Name" className="form-control" />
                     </div>
-                      <div className="form-group">
+                    <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <input type="email" id="email" placeholder="Email" className="form-control" />
                     </div>
-                    <div className="input_field_wrapper form-group col-md-12">
-                        <label htmlFor="projectDetails">Collection Brief</label>
-                        <textarea id="projectDetails" cols="30" rows="10" placeholder="Collection Brief"></textarea>
-                    </div>
                 </div>
 
-                <div className="form_widgets form_check">
-                    <h5>BLOCKCHAIN/PLATFORM</h5>
-                    {/*   <label>Binance Smart Chain
-                        <input type="radio" name="platfrom" value="binance" />
-                    </label>*/}
-                    <label>Sonic Blockchain
-                        <input type="radio" name="platfrom" value="sonic" />
-                    </label>
-                    {/*  <label>Ethereum
-                        <input type="radio" name="platfrom" value="ethereum" />
-                    </label>
-                    <label>Polygon (Matic)
-                        <input type="radio" name="platfrom" value="polygon" />
-                    </label>*/}
-                </div>
-
-                <div className="form_widgets form_check">
-                    <h5>PROJECT STATUS</h5>
-
-                    <label>Just an initial idea
-                        <input type="radio" name="projectStatus" value="initial" />
-                    </label>
-                    <label>Idea with White Paper
-                        <input type="radio" name="projectStatus" value="whitePaper" />
-                    </label>
-                    <label>In early development
-                        <input type="radio" name="projectStatus" value="development" />
-                    </label>
-                    <label>Ready to launch
-                        <input type="radio" name="projectStatus" value="production" />
-                    </label>
-                </div>
-
-                <div className="form_widgets form_check">
-                    <h5>HAVE YOU ALREADY RAISED FUNDS ?</h5>
-
-                    <label>Yes
-                        <input type="radio" name="funds" value="yes" />
-                    </label>
-                    <label>No
-                        <input type="radio" name="funds" value="no" />
-                    </label>
-                </div>
-
-                <div className="form_widgets form_check">
-                    <h5>IS YOUR TEAM ANON OR PUBLIC</h5>
-                    <label>Anon
-                        <input type="radio" name="visibility" value="anon" />
-                    </label>
-                    <label>Fully Public
-                        <input type="radio" name="visibility" value="Public" />
-                    </label>
-                    <label>Mixed
-                        <input type="radio" name="visibility" value="mixed" />
-                    </label>
-                </div>
-
+                {/* NFT Layer Management */}
                 <div className="form_widgets">
-                    <div className="form-group">
-                        <label htmlFor="fundterget">TARGET RAISE</label>
-                        <input type="text" id="fundterget" placeholder="How much are you planning on raising on the IGO?" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="projectLink">COLLECTION WEBSITE / LINK TO WHITEPAPER</label>
-                        <input type="text" id="projectLink" placeholder="Enter link" className="form-control" />
-                    </div>
+                    <h5>NFT Layer Management</h5>
+                    {layers.map((layer, index) => (
+                        <div key={index} className="layer-group">
+                            <input
+                                type="text"
+                                placeholder="Layer Name"
+                                value={layer.name}
+                                onChange={(e) => handleTraitChange(index, "name", e.target.value)}
+                                className="form-control"
+                            />
+                            <input
+                                type="number"
+                                placeholder="Rarity Percentage"
+                                value={layer.rarity}
+                                onChange={(e) => handleTraitChange(index, "rarity", e.target.value)}
+                                className="form-control"
+                            />
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => handleLayerUpload(e, index)}
+                                className="form-control"
+                            />
+                        </div>
+                    ))}
+                    <Button variant="blue" onClick={addLayer}>
+                        <FaUpload /> Add Layer
+                    </Button>
                 </div>
 
+                {/* NFT Generation Options */}
+                <div className="form_widgets">
+                    <h5>Generate NFTs</h5>
+                    <input
+                        type="number"
+                        value={nftCount}
+                        onChange={(e) => setNftCount(e.target.value)}
+                        className="form-control"
+                        placeholder="Number of NFTs to Generate"
+                    />
+                    <Button variant="blue" onClick={generateNFTs}>
+                        <FaMagic /> Generate & Upload
+                    </Button>
+                </div>
+
+                {/* Social Links */}
                 <div className="form_widgets">
                     <div className="form-group">
                         <label htmlFor="telegram">TELEGRAM GROUP</label>
                         <div className="input_with_icon">
-                            <div className="input_social_icon">
-                                <FaTelegramPlane />
-                            </div>
+                            <div className="input_social_icon"><FaTelegramPlane /></div>
                             <input type="text" id="telegram" placeholder="Enter telegram group link" className="form-control" />
                         </div>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="telegram">COLLECTION Twitter</label>
+                        <label htmlFor="twitter">COLLECTION Twitter</label>
                         <div className="input_with_icon">
-                            <div className="input_social_icon">
-                                <FaTwitter />
-                            </div>
+                            <div className="input_social_icon"><FaTwitter /></div>
                             <input type="text" id="twitter" placeholder="Enter twitter link" className="form-control" />
                         </div>
                     </div>
@@ -115,7 +155,7 @@ const ApplyForm = () => {
                 </Button>
             </form>
         </ApplyFormStyleWrapper>
-    )
-}
+    );
+};
 
 export default ApplyForm;
