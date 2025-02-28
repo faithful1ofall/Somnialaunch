1ecedd6d6474fe1c47aef3cc49f6d0589b3df10e
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-02-28 14:49:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         {metamaskModal && <MetamaskModal />}
         <Header />
         <Banner />
-        <About />
+          {/*  <About /> */}
         <NextProjects />
         <PreviousProjects />
           {/*   <Statistics />
