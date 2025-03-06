3dde882ba1aa3ae46696942e7f2f3a93f494b641
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-06 13:01:05 +0100

    Update igo-apply.js

--- pages/igo-apply.js
+++ pages/igo-apply.js
@@ -4,7 +4,7 @@ import SEO from "@components/SEO";
 import Layout from "@components/layout";
 import WalletModal from "@components/modal/walletModal/WalletModal";
 import MetamaskModal from "@components/modal/metamaskModal/MetamaskModal";
-import Header from "@sections/Header/v2";
+import Header from "@sections/Header/v1";
 import PageHeader from "@sections/IGOApplyPage/PageHeader";
 import IGOApply from "@sections/IGOApplyPage";
 import Footer from "@sections/Footer/v1";
