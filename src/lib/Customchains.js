cb213c59671ccf76499ae5f6659bef637c445227
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-26 11:24:05 +0100

    Update Customchains.js

--- src/lib/Customchains.js
+++ src/lib/Customchains.js
@@ -1,12 +1,12 @@
 import { defineChain } from "thirdweb/chains";
 
 
-export const sonicTestnet = defineChain({
-id: 57054,
-rpc: "https://rpc.blaze.soniclabs.com",
+export const lineaTestnet = defineChain({
+id: 59141,
+rpc: "https://rpc.sepolia.linea.build",
 nativeCurrency: {
-name: "Sonic",
-symbol: "S",
+name: "linea-sepolia",
+symbol: "ETH",
 decimals: 18,
 },
 });
