991521b5cb9950933868e0377a4963a3f95f321c
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-04-27 16:42:34 +0100

    Update Banner.jsx

--- src/sections/Banner/v1/Banner.jsx
+++ src/sections/Banner/v1/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
               alt="banner icon"
             />
             <h1 className="banner-title">
-              Mini Launch Metaverse Web 3.0 NFT Agent Launchpad &amp; AI
+              Root Launch Metaverse Web 3.0 NFT Agent Launchpad &amp; AI
             </h1>
             <div className="description">
               The next generation A.I ecosystem for NFT AI Agents
